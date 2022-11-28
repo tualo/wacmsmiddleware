2f27592a430cb05d2508b7820247e13c99ef636d
Steffen Borchmann <steffen.borchmann@tualo.de>
2022-11-28 12:01:58 +0100

    New class

---
+++ src/Login.php
@@ -0,0 +1,9 @@
+<?php 
+    namespace Tualo\Office\WaCmsMiddleware;
+    use Tualo\Office\ContentManagementSystem\CMSMiddleware;
+
+    class Login extends CMSMiddleware {
+        public static function run(&$request,&$result){
+            $result['SBO'] = time();
+        }
+    } 
