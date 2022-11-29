614cc7652b63e7ceec78ed6c103544712e775717
Steffen Borchmann <steffen.borchmann@tualo.de>
2022-11-29 08:19:10 +0100

    ad login

--- src/Login.php
+++ src/Login.php
@@ -5,5 +5,26 @@
     class Login extends CMSMiddleware {
         public static function run(&$request,&$result){
             $result['sbo'] = time();
+            if ($_SESSION['wa_session']['login']['loggedIn']===false){
+                // isset 
+                if (
+                    isset($_REQUEST['loginFormID']) 
+                        && ($_SESSION['wa_session']['login']['loginFormID']==$_REQUEST['loginFormID'])
+                ){
+                    if (
+                        isset($_REQUEST[$_SESSION['wa_session']['login']['lastformId']]) 
+                        && ($_SESSION['wa_session']['login']['loginFormID']==$_REQUEST[$_SESSION['wa_session']['login']['lastformId']])
+                    ){
+                        if ($_REQUEST[$_SESSION['wa_session']['login']['lastformId']] == 'sbo'){
+                            $_SESSION['wa_session']['login']['loggedIn']=true;   
+                        } 
+                    } else {
+                        $_SESSION['wa_session']['login']['loginFormError']++;
+                    }
+                } else{
+                   //  $_SESSION['wa_session']['login']['loginFormError']++;
+                }
+            }
+            $result['loginFormError']=$_SESSION['wa_session']['login']['loginFormError'];
         }
     } 
