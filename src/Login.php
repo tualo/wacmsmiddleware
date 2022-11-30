e9d0f5279c59811c1c96c241e2db01355c8403f9
Steffen Borchmann <steffen.borchmann@tualo.de>
2022-11-30 11:17:34 +0100

    w

--- src/Login.php
+++ src/Login.php
@@ -10,10 +10,25 @@
             if( 
                 isset($_REQUEST[$_SESSION['wa_session']['login']['usrOldID']]) 
                     && isset($_REQUEST[$_SESSION['wa_session']['login']['pwOldID']])
-            ){
+            ){  
+                // checking USR DATA
+                if( $_REQUEST[$_SESSION['wa_session']['login']['usrOldID']] == 'ABCDEFGH' // USR
+                        &&  $_REQUEST[$_SESSION['wa_session']['login']['pwOldID']]== 'ABCDEFGH' // PW
+                ) {
+                    $_SESSION['wa_session']['login']['loggedIn']=TRUE;
+                    $_SESSION['wa_session']['login']['user']='Karl Knall';
+                    $_SESSION['wa_session']['login']['role']='OberAdmin';
+                    header('Location: ./wa');
+                } else {
+
+
+                }
+
                 $result['message']=' Formularfelder vorhanden -> passt -';
+
             } else {
                 $result['message']=' Formularfelder NICHT vorhanden -> passt NICHT -';
+                $_SESSION['wa_session']['login']['loginFormError']++;
             }
            /* if ($_SESSION['wa_session']['login']['loggedIn']===false){
                 // isset 
