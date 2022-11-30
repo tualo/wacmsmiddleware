54a747198e5ed81b5f02a411f78de878cc471c90
Steffen Borchmann <steffen.borchmann@tualo.de>
2022-11-30 10:26:07 +0100

    add message

--- src/Login.php
+++ src/Login.php
@@ -7,13 +7,21 @@
             @session_start();
             $result['sbo'] = time();
             $result['loginstart']=$_SESSION['wa_session']['login']['formID'].'-----'.$_REQUEST['formID'];
+            if( 
+                isset($_REQUEST[$_SESSION['wa_session']['login']['usrOldID']]) 
+                    && isset($_REQUEST[$_SESSION['wa_session']['login']['pwOldID']])
+            ){
+                $result['message']=' Formularfelder vorhanden passt -';
+            } else {
+                $result['message']=' Formularfelder NICHT vorhanden passt -';
+            }
             if ($_SESSION['wa_session']['login']['loggedIn']===false){
                 // isset 
                 if (
                     isset($_REQUEST['formID']) 
                         && ($_SESSION['wa_session']['login']['formID']==$_REQUEST['formID'])
                 ){
-                    $result['message']=' Formfeld passt -'.$_SESSION['wa_session']['login']['formID'].' vs. '.$_REQUEST['formID'];
+                    // $result['message']=' Formfeld passt -'.$_SESSION['wa_session']['login']['formID'].' vs. '.$_REQUEST['formID'];
                     if (
                         isset($_REQUEST[$_SESSION['wa_session']['login']['lastformId']]) 
                         && ($_SESSION['wa_session']['login']['formId']==$_REQUEST[$_SESSION['wa_session']['login']['lastformId']])
@@ -26,7 +34,7 @@
                     }
                 } else{
                    $_SESSION['wa_session']['login']['loginFormError']++;
-                   $result['message']=' Formfeld passt NICHT!-'.$_SESSION['wa_session']['login']['formID'].' vs. '.$_REQUEST['formID'];
+                   // $result['message']=' Formfeld passt NICHT!-'.$_SESSION['wa_session']['login']['formID'].' vs. '.$_REQUEST['formID'];
                 }
             }
             $result['loginFormError']=$_SESSION['wa_session']['login']['loginFormError'];
