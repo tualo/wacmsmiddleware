4b05240699bd9ecf34836f9beb9c79d708acb502
Steffen Borchmann <steffen.borchmann@tualo.de>
2022-12-07 13:19:13 +0100

    new class

---
+++ src/History.php
@@ -0,0 +1,19 @@
+<?php 
+    namespace Tualo\Office\WaCmsMiddleware;
+    use Tualo\Office\ContentManagementSystem\CMSMiddleware;
+    use Tualo\Office\Basic\TualoApplication as App;
+
+    class History extends CMSMiddleware {
+        public static function run(&$request,&$result){
+            @session_start();
+            try{
+                $db  = App::get('session')->getDB();
+                $history=$db->singleRow("select  * from wa_action_log order by createtime desc",[]);
+                $result['history']=$history;
+            }catch(\Exception $e){
+                 $result['message']=$result['message'].$e->getMessage();
+            }
+            session_commit();
+        }
+    }
+
